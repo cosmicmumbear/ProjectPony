13ff399f0b51eed0a8f2bdfb35dae49dada4395e
cosmicmumbear <105939075+cosmicmumbear@users.noreply.github.com>
2022-05-21 19:30:36 +0200

    move folder in unity to proper location

---
+++ Scripts/LevelManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+   [SerializeField] float sceneLoadDelay = 1.5f;
+   //ScoreKeeper scoreKeeper;
+
+//    void Awake() 
+//    {
+//        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+//    }
+   public void LoadGame()
+   {
+       //scoreKeeper.ResetScore();
+       SceneManager.LoadScene(2);
+   }
+
+    public void LoadMainMenu()
+   {
+       SceneManager.LoadScene(0);
+   }
+
+     public void LoadAbout()
+   {
+       SceneManager.LoadScene(1);
+   }
+
+    public void LoadGameOver()
+   {
+       StartCoroutine(WaitAndLoad(2, sceneLoadDelay));
+   }
+
+    public void QuitGame()
+   {
+       Debug.Log("Quit");
+       Application.Quit();
+   }
+
+   IEnumerator WaitAndLoad(int sceneIndex, float delay) 
+   {
+       yield return new WaitForSeconds(delay);
+       SceneManager.LoadScene(sceneIndex);
+   }
+}
